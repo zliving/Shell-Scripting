5dc2e85985ca2d0e32152e6e079157f28f7be52b
Zach Livingston <zachary.n.livingston@gmail.com>
2016-09-20 12:03:32 -0500

    Greeting SC
    Shell Script greets the current user logged in depending on what time it is

---
+++ greeting.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+temph=`date | cut -c 12-13`
+dat=`date +"%A %d in %B of %Y (%r)"`
+
+mess="Hello $LOGNAME!"
+
+if [ $temph -lt 12 ]
+then
+    mess="Good Morning $LOGNAME, Have nice day!"
+fi
+
+if [ $temph -gt 12 -a $temph -le 16 ]
+then
+    mess="Good Afternoon $LOGNAME"
+fi
+
+if [ $temph -gt 16 -a $temph -le 18 ]
+then
+    mess="Good Evening $LOGNAME"
+fi
+
+if [ $temph -gt 18 -a $temph -le 24 ]
+then
+    mess="Hello $LOGNAME"
+fi
+
+echo -e "$mess\nThis is $dat"
+
